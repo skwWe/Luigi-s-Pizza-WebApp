d5dbeee7d25f4b511dfb8ae24d910187c63055dd
«Семён» <=>
2025-05-20 00:59:30 +0300

    Added interface on Registration screen

--- PizzaWebApp/obj/Debug/net8.0/PizzaWebApp.AssemblyInfo.cs
+++ PizzaWebApp/obj/Debug/net8.0/PizzaWebApp.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PizzaWebApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9d78e84c40752a47e5f3528d07411f2dcf70836b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e8b250391fe1011908200aae3532bfe987964a2d")]
 [assembly: System.Reflection.AssemblyProductAttribute("PizzaWebApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PizzaWebApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
