f18280c5dce0bf78a2bbef908280d54953d535d3
OrokanaKitsyne <orokana011899@gmail.com>
2025-06-01 20:51:44 +0300

    объедение сайдбара с главной страницей

--- PizzaWebApp/obj/Debug/net8.0/PizzaWebApp.AssemblyInfo.cs
+++ PizzaWebApp/obj/Debug/net8.0/PizzaWebApp.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PizzaWebApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c6e113a98a123564dc940f3a7c3f51558e92bd3b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+934e6bacb6dfb7f3af1445833410bd69ba2f2b2d")]
 [assembly: System.Reflection.AssemblyProductAttribute("PizzaWebApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PizzaWebApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
