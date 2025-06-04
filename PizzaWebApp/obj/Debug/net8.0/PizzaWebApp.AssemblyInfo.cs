051094385eba814038c661a8932051b1f9891077
«Семён» <=>
2025-06-04 19:33:27 +0300

    баг фикс

--- PizzaWebApp/obj/Debug/net8.0/PizzaWebApp.AssemblyInfo.cs
+++ PizzaWebApp/obj/Debug/net8.0/PizzaWebApp.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PizzaWebApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7d04a41690d5783b0f1d46d3d4084797187df281")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ddef157e871e183a5cbf8eeff5b0c8e28897423c")]
 [assembly: System.Reflection.AssemblyProductAttribute("PizzaWebApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PizzaWebApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
