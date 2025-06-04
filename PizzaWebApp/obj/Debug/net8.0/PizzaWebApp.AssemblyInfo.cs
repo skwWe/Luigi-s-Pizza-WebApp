ec927b294d5f543e4a67bc0299b906858967017f
akukina65 <akukina65@yandex.ru>
2025-06-04 23:51:06 +0300

    Добавление всей документации

--- PizzaWebApp/obj/Debug/net8.0/PizzaWebApp.AssemblyInfo.cs
+++ PizzaWebApp/obj/Debug/net8.0/PizzaWebApp.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PizzaWebApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a1c06403d0d32138b6dcbcec56fb1647060c6900")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+664d54898f287c02a577d176bdb39bf5b7e0dfaa")]
 [assembly: System.Reflection.AssemblyProductAttribute("PizzaWebApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PizzaWebApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
